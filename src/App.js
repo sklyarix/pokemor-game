f25e51ea881fa0b637a8c8bb5750d34ea155d9bf
sklyarix <work.sklyar@yandex.ru>
2021-01-28 22:04:26 +0300

    Homework 2
    Интернет отрбули((
    Пришлось с телефона раздавать)))

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import bg3 from './components/img/bg3.jpg'
+import Header from './components/Header/Header';
+import Layout from './components/Layout/Layout';
+import Footer from './components/Footer/Footer';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello world !
-        </p>
-      </header>
-    </div>
+    <>
+      <Header title = "This is title" descr = "This is Description!"/>
+      <Layout title = "This is title" descr = "This is Description!" id = "layout1" urlBg = {bg3} />
+      <Layout title = "This is title" descr = "This is Description!" id = "layout2" colorBg = "#ff0000"/>
+      <Layout title = "This is title" descr = "This is Description!" id = "layout3" urlBg = {bg3}/>
+      <Footer/>
+
+    </>
   );
 }
 
